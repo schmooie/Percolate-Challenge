55799f49524a10c2e59884fc199389a24aaa82f0
Sam Lau <slau234@gmail.com>
2014-08-05 18:25:03 -0400

    Successfully sending emails on form submit.

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 var app = express();
+
+var transporter = nodemailer.createTransport({
+	service: 'Gmail',
+	auth: {
+		user: 'dummyuser5000@gmail.com',
+		pass: 'duganIsMyName1'
+	}
+});
+
+var mailOptions = {
+	from: 'Dum Dum Dugan <dummyuser5000@gmail.com>',
+	subject: 'You\'re Percolating!'
+};
 
 app.use(express.static(__dirname + '/client'));
 app.use(bodyParser());
@@ -8,7 +22,20 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.post('/request-demo', function(req, res) {
-	console.log(req.body);
+	var firstName = req.body.first;
+	var lastName = req.body.last;
+	var email = req.body.email;
+
+	mailOptions.to = email;
+	mailOptions.html = 'Hi ' + firstName + ' ' + lastName +'!<br>This is some text. Right?';
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Message sent: ' + info.response);
+		}
+	});
 });
 
 app.get('/', function(req, res) {
