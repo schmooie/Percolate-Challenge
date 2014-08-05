cad4c0810b0e6d02d15a333d82c6e284781f3d9b
Sam Lau <slau234@gmail.com>
2014-08-05 18:38:38 -0400

    Added some Dickens to class up the joint.

--- server.js
+++ server.js
@@ -16,6 +16,26 @@ var mailOptions = {
 	subject: 'You\'re Percolating!'
 };
 
+var twoCities = 'Here\'s some Dickens for you.<br><br>' +
+		'It was the best of times,<br>' +
+		'it was the worst of times,<br>' +
+		'it was the age of wisdom,<br>' +
+		'it was the age of foolishness,<br>' +
+		'it was the epoch of belief,<br>' +
+		'it was the epoch of incredulity,<br>' +
+		'it was the season of Light,<br>' +
+		'it was the season of Darkness,<br>' +
+		'it was the spring of hope,<br>' +
+		'it was the winter of despair,<br>' +
+		'we had everything before us,<br>' +
+		'we had nothing before us,<br>' +
+		'we were all going direct to Heaven,<br>' +
+		'we were all going direct the other way--<br>' +
+		'in short, the period was so far like the present period, that some of<br>' +
+		'its noisiest authorities insisted on its being received, for good or for<br>' +
+		'evil, in the superlative degree of comparison only.<br><br>' +
+		'From <a href="http://percolate.com">Percolate</a>';
+
 app.use(express.static(__dirname + '/client'));
 app.use(bodyParser());
 app.engine('html', require('ejs').renderFile);
@@ -27,13 +47,14 @@ app.post('/request-demo', function(req, res) {
 	var email = req.body.email;
 
 	mailOptions.to = email;
-	mailOptions.html = 'Hi ' + firstName + ' ' + lastName +'!<br>This is some text. Right?';
+	mailOptions.html = 'Hi ' + firstName + ' ' + lastName +'!<br><br>' + twoCities;
 
 	transporter.sendMail(mailOptions, function (error, info) {
 		if (error) {
 			console.log(error);
 		} else {
 			console.log('Message sent: ' + info.response);
+			res.send('Email sent!');
 		}
 	});
 });
