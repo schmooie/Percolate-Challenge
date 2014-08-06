e66d6306bdbc34b74736281d77dabb61fdc21599
Sam Lau <slau234@gmail.com>
2014-08-06 03:24:35 -0400

    Form validates and provides detailed feedback upon submit.

--- client/app.js
+++ client/app.js
@@ -1,39 +1,26 @@
 var app = angular.module('percolate', []);
 
-app.controller('MainCtrl', ['$scope', '$http', 'Email',
-  function($scope, $http, Email) {
-    $scope.formInvalid = false;
+app.controller('MainCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $scope.submitted = false;
     $scope.user = {};
-    $scope.submitForm = function() {
-    	// $scope.formInvalid = false;
+    $scope.emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-      if ($scope.formInvalid === false) {
+    $scope.submitForm = function(isValid) {
+      $scope.submitted = true;
+      if (isValid) {
         $http.post('/request-demo', $scope.user)
           .success(function(data) {
             console.log(data);
           });
       } else {
-        $scope.formInvalid = true;
-        console.log('Bad!');
-      }
-    };
-  }
-]);
-
-app.service('Email', [
-
-  function() {
-    return {
-      validate: function(email) {
-        var validEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return validEmail.test(email);
+        console.log('Bad credentials!');
       }
     };
   }
 ]);
 
 app.directive('emailInput', [
-
   function() {
     return {
       restrict: 'A'
@@ -42,7 +29,6 @@ app.directive('emailInput', [
 ]);
 
 app.directive('firstNameInput', [
-
   function() {
     return {
       restrict: 'A'
@@ -51,7 +37,6 @@ app.directive('firstNameInput', [
 ]);
 
 app.directive('lastNameInput', [
-
   function() {
     return {
       restrict: 'A'
@@ -59,41 +44,31 @@ app.directive('lastNameInput', [
   }
 ]);
 
-app.directive('validateInput', ['Email',
-  function(Email) {
+app.directive('validateInput', [
+  function() {
     return {
       restrict: 'A',
       require: 'ngModel',
       link: function(scope, elem, attr, ctrl) {
-      	elem.on('focus', function() {
-      		elem.removeClass('bad-credentials');
-      	});
+
+        elem.on('focus', function() {
+          elem.removeClass('bad-credentials');
+        });
 
         elem.on('blur', function() {
-        	console.log(elem[0].value);
+          console.log(elem[0].value);
           if (attr.hasOwnProperty('emailInput')) {
-            if (!Email.validate(elem[0].value)) {
+            console.log('Email good?', scope.emailRegExp.test(elem[0].value));
+            if (!(scope.emailRegExp.test(elem[0].value))) {
               elem.addClass('bad-credentials');
-              ctrl.$setValidity('invalid', true);
-              scope.formInvalid = true;
-            } else {
-            	ctrl.$setValidity('invalid', false);
             }
           } else if (attr.hasOwnProperty('firstNameInput')) {
-            if (!(/.+/gi).test(elem[0].value)) {
+            if (!(/.+/gi.test(elem[0].value))) {
               elem.addClass('bad-credentials');
-              ctrl.$setValidity('invalid', true);
-              scope.formInvalid = true;
-            } else {
-            	ctrl.$setValidity('invalid', false);
             }
           } else if (attr.hasOwnProperty('lastNameInput')) {
-            if (!(/.+/gi).test(elem[0].value)) {
+            if (!(/.+/gi.test(elem[0].value))) {
               elem.addClass('bad-credentials');
-              ctrl.$setValidity('invalid', true);
-              scope.formInvalid = true;
-            } else {
-            	ctrl.$setValidity('invalid', false);
             }
           }
         });
